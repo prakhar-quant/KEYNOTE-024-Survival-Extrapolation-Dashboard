be507932219ae6344bf3f2f3ec372041c9a498eb
Prakhar Gupta <prakhargupta050829@gmail.com>
2026-08-03 01:59:03 +0530

    Add deployment script for shinyapps.io
    Added deploy.R to simplify deployment of the KEYNOTE-024 Survival Extrapolation Dashboard to shinyapps.io using the rsconnect package. The script includes deployment commands and project configuration for reproducible updates.

---
+++ deploy.R
@@ -0,0 +1,4 @@
+rsconnect::setAccountInfo(name='prakhargupta', token='B42F787F1501F517D54D9EE0EADCBD8C', secret='2JbA8XFf2wBuDY+3EQbL1a6Q9kaZZRiMGFdG9NW3')
+packageVersion("rlang")
+rsconnect::deployApp()
+rsconnect::showLogs()
